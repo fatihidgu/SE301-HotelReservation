5369c491755351b422cb5bb7589d64059bd63c04
fatih <fatihidgu@gmail.com>
2020-01-15 16:26:33 +0300

    .

--- project/src/Validator/PhoneValidator.java
+++ project/src/Validator/PhoneValidator.java
@@ -1,39 +1,31 @@
 package Validator;
 
 
+
 import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.validator.FacesValidator;
 import javax.faces.validator.Validator;
 import javax.faces.validator.ValidatorException;
-
-
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @FacesValidator("phoneValidator")
 public class PhoneValidator implements Validator {
-
-    private String pattern;
+    private static final Pattern PHONE_PATTERN = Pattern.compile("(\\d{2})(\\d{2})(\\d{3})");
 
     @Override
     public void validate(FacesContext context, UIComponent component,
                          Object value) throws ValidatorException {
-        PhonePattern phonePattern = PhonePattern.getPhonePattern(pattern);
-        if (!phonePattern.match(value.toString())) {
+        Matcher matcher = PHONE_PATTERN.matcher(value.toString());
+        if (!matcher.matches()) {
             FacesMessage msg =
                     new FacesMessage("Invalid phone format",
-                            String.format("invalid input: %s, The valid format regex: %s",
-                                    value, phonePattern.getPattern()));
+                            String.format("Invalid phone number: %s, Example : 1212123",
+                                    value));
             msg.setSeverity(FacesMessage.SEVERITY_ERROR);
             throw new ValidatorException(msg);
         }
-    }
-
-    public String getPattern() {
-        return pattern;
-    }
-
-    public void setPattern(String pattern) {
-        this.pattern = pattern;
     }
 }
